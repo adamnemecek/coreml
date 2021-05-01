b80ff155031c97696a94390c60f36cef0aad7e13
adamnemecek <adamnemecek@gmail.com>
2021-05-01 14:43:14 -0700

    coreml

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![link(name = "CoreML", kind = "framework")]
+
 mod ml_array_batch_provider;
 pub use ml_array_batch_provider::*;
 
@@ -124,3 +126,58 @@ pub use ml_update_task::*;
 mod ml_writable;
 pub use ml_writable::*;
 
+
+#[macro_use]
+macro_rules! foreign_obj_type {
+    {type CType = $raw_ident:ident;
+    pub struct $owned_ident:ident;
+    pub struct $ref_ident:ident;
+    type ParentType = $parent_ref:ident;
+    } => {
+        foreign_obj_type! {
+            type CType = $raw_ident;
+            pub struct $owned_ident;
+            pub struct $ref_ident;
+        }
+
+        impl ::std::ops::Deref for $ref_ident {
+            type Target = $parent_ref;
+
+            fn deref(&self) -> &$parent_ref {
+                unsafe { &*(self as *const $ref_ident as *const $parent_ref)  }
+            }
+        }
+    };
+    {type CType = $raw_ident:ident;
+    pub struct $owned_ident:ident;
+    pub struct $ref_ident:ident;
+    } => {
+        foreign_type! {
+            type CType = $raw_ident;
+            fn drop = crate::obj_drop;
+            fn clone = crate::obj_clone;
+            pub struct $owned_ident;
+            pub struct $ref_ident;
+        }
+
+        unsafe impl ::objc::Message for $raw_ident {
+        }
+        unsafe impl ::objc::Message for $ref_ident {
+        }
+
+        impl ::std::fmt::Debug for $ref_ident {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                unsafe {
+                    let string: *mut ::objc::runtime::Object = msg_send![self, debugDescription];
+                    write!(f, "{}", crate::nsstring_as_str(&*string))
+                }
+            }
+        }
+
+        impl ::std::fmt::Debug for $owned_ident {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                ::std::ops::Deref::deref(self).fmt(f)
+            }
+        }
+    };
+}
